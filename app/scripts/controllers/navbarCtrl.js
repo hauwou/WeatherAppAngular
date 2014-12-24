d3a17b240bb13d217d93d7121b54408443c28cb6
hauwou <hauwou@gmail.com>
2014-12-23 18:06:10 -0800

    Minor changes.

---
+++ app/scripts/controllers/navbarCtrl.js
@@ -0,0 +1,16 @@
+angular.module('weatherApp')
+  .controller('NavBarCtrl', ['$scope', '$routeParams', '$location', function ($scope, $routeParams, $location) { 
+    
+      $scope.isActive = function(currentUrl){
+          if (currentUrl === $location.path()){
+            return "active";
+          }
+          else {
+            return "";
+          }
+         
+    }
+      
+  
+  }
+                              ]);
